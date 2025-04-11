b606a693d4b4d84938fad3b85275bc769f3c714f
LAZZARONI.21130 <lazzaroni.daniel.studente@itispaleocapa.it>
2025-04-11 10:00:30 +0200

    prototipo

--- script.js
+++ script.js
@@ -2,14 +2,14 @@ function addTitolo(event) {
     event.preventDefault();
 
     titoliCulturali = document.getElementById("titoliCulturali");
-    titoliCulturali.innerHTML += "<div><input type='text' placeholder='titolo' id='titolo[]'> <input type='text' placeholder='anno' id='anno'> <button onclick='remove(this)'>-</button></br></div>";
+    titoliCulturali.innerHTML += "<div><input type='text' placeholder='titolo' class='titolo'  name='titolo[]'> <input type='text' placeholder='anno' id='anno'  name='anno[]'> <button onclick='remove(this)'>-</button></br></div>";
 }
 
 function addEsperienza(event){
     event.preventDefault();
 
     formEsperienze = document.getElementById("formEsperienze");
-    formEsperienze.innerHTML += "<div><input type='text' placeholder='azienda' id='azienda'> <input type='text' placeholder='dal' id='dal'> <input type='text' placeholder='al' id='al'> <button onclick='remove(this)'>-</button></br></div>";
+    formEsperienze.innerHTML += "<div><input type='text' placeholder='azienda' class='azienda'  name='azienda[]'> <input type='text' placeholder='dal' id='dal'  name='dal[]'> <input type='text' placeholder='al' id='al'  name='al[]'> <button onclick='remove(this)'>-</button></br></div>";
 }
 
 function remove(id){
